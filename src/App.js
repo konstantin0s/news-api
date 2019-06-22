708c7e74cc5e798ad0c7b248c2d16ce8a0f29f83
konstantin0s <constantintofan85@gmail.com>
2019-06-22 12:11:58 +0200

    News component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import News from './components/News';
 
 class App extends Component {
   constructor(props) {
@@ -8,8 +9,19 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <h1>NEws API :)</h1>
+      <div className="App container-fluid">
+        <div className="navbar-fixed">
+          <nav>
+            <div className="nav-wrapper indigo lighten-4">
+              <a href="#" className="brand-logo center">My Feed</a>
+            </div>
+          </nav>
+        </div>
+        <div className="row">
+          <div className="col 8">
+
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/News.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class News extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div className="news">
+                <h2>News component</h2>
+            </div>
+        );
+    }
+}
+
+export default News;
