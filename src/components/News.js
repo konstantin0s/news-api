46c95111d4da20e6d6ca557d53ec8035148cf382
konstantin0s <constantintofan85@gmail.com>
2019-06-22 12:56:03 +0200

    Add News component

--- src/components/News.js
+++ src/components/News.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import NewSingle from './NewSingle';
+import axios from 'axios';
 
 class News extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            news: []
+        };
     }
+
+    componentDidMount() {
+        const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=${process.env.REACT_APP_API_KEY}`;
+        axios.get(url)
+            .then((res) => {
+                return res.json();
+            })
+            .then(data => {
+                this.setState({
+                    news: data.articles
+                })
+            })
+            .catch(err => console.log(err));
+    }
+
+    renderItems = () => {
+        return this.state.news.map((item) => (
+            <NewSingle key={item.url} item={item} />
+        ))
+    }
+
     render() {
         return (
-            <div className="news">
-                <h2>News component</h2>
+            <div className="row">
+                {this.renderItems()}
             </div>
         );
     }
