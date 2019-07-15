9411e55e7f7ddde2981b2265e720abaa8e0cdfa1
konstantin0s <constantintofan85@gmail.com>
2019-07-15 19:15:55 +0200

    Updates

---
+++ precache-manifest.ae4989fd9bfb9104ca77d814cacbaa43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f1b244511a2e45f944781a20646c0f",
+    "url": "/news-api/index.html"
+  },
+  {
+    "revision": "3400f6f6423bca825bb1",
+    "url": "/news-api/static/css/main.5047e0ac.chunk.css"
+  },
+  {
+    "revision": "e28347e3d491e076f314",
+    "url": "/news-api/static/js/2.32345e5b.chunk.js"
+  },
+  {
+    "revision": "3400f6f6423bca825bb1",
+    "url": "/news-api/static/js/main.6632abf4.chunk.js"
+  },
+  {
+    "revision": "91fb6225781f0b454dcd",
+    "url": "/news-api/static/js/runtime~main.45697807.js"
+  }
+]);
